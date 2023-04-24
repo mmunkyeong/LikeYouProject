d19b25b4b8c80321672febe70a4b62720b2ca0e8
mmunkyeong <jmk4379@naver.com>
2023-04-24 09:38:56 +0900

    테스트 유연성

--- src/test/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonControllerTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonControllerTests.java
@@ -4,6 +4,8 @@ package com.ll.gramgram.boundedContext.likeablePerson.controller;
 import com.ll.gramgram.base.appConfig.AppConfig;
 import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonService;
+import com.ll.gramgram.boundedContext.member.entity.Member;
+import com.ll.gramgram.boundedContext.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
@@ -32,6 +35,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public class LikeablePersonControllerTests {
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private MemberService memberService;
+
     @Autowired
     private LikeablePersonService likeablePersonService;
     @Test
@@ -282,9 +289,16 @@ public class LikeablePersonControllerTests {
     }
 
     @Test
-    @DisplayName("한 회원은 호감표시를 할 수 있는 최대 인원이 정해져 있다.")
+    @DisplayName("한 회원은 호감표시 할 수 있는 최대 인원이 정해져 있다.")
     @WithUserDetails("user5")
     void t012() throws Exception {
+        Member memberUser5= memberService.findByUsername("user5").get();
+
+        IntStream.range(0, (int) AppConfig.getLikeablePersonFromMax())
+                .forEach(index -> {
+                    likeablePersonService.like(memberUser5, "insta_user%30d".formatted(index), 1);
+                });
+
         // WHEN
         ResultActions resultActions = mvc
                 .perform(post("/likeablePerson/like")
