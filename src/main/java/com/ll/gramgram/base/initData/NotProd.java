5459ee5876765015ff760d52ce5015ca3fb5cb15
mmunkyeong <jmk4379@naver.com>
2023-04-26 13:41:47 +0900

    feat: 성별 수정 기능

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -4,6 +4,7 @@ import com.ll.gramgram.boundedContext.instaMember.service.InstaMemberService;
 import com.ll.gramgram.boundedContext.likeablePerson.service.LikeablePersonService;
 import com.ll.gramgram.boundedContext.member.entity.Member;
 import com.ll.gramgram.boundedContext.member.service.MemberService;
+import jakarta.transaction.Transactional;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,24 +19,28 @@ public class NotProd {
             InstaMemberService instaMemberService,
             LikeablePersonService likeablePersonService
     ) {
-        return args -> {
-            Member memberAdmin = memberService.join("admin", "1234").getData();
-            Member memberUser1 = memberService.join("user1", "1234").getData();
-            Member memberUser2 = memberService.join("user2", "1234").getData();
-            Member memberUser3 = memberService.join("user3", "1234").getData();
-            Member memberUser4 = memberService.join("user4", "1234").getData();
-            Member memberUser5 = memberService.join("user5","1234").getData();
-            Member memberUser6ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2733191015").getData();
-            Member memberUser7ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__4eTJY8rdhl1uv-gv5PP8YgNQ3UcoCRWFa5_zdJOAvbU").getData();
-            Member memberUser8ByGOOGLE = memberService.whenSocialLogin("GOOGLE", "GOOGLE__115052980621736075605").getData();
+        return new CommandLineRunner() {
+            @Override
+            @Transactional
+            public void run(String... args) throws Exception {
+                Member memberAdmin = memberService.join("admin", "1234").getData();
+                Member memberUser1 = memberService.join("user1", "1234").getData();
+                Member memberUser2 = memberService.join("user2", "1234").getData();
+                Member memberUser3 = memberService.join("user3", "1234").getData();
+                Member memberUser4 = memberService.join("user4", "1234").getData();
+                Member memberUser5 = memberService.join("user5", "1234").getData();
+                Member memberUser6ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2733191015").getData();
+                Member memberUser7ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__4eTJY8rdhl1uv-gv5PP8YgNQ3UcoCRWFa5_zdJOAvbU").getData();
+                Member memberUser8ByGOOGLE = memberService.whenSocialLogin("GOOGLE", "GOOGLE__115052980621736075605").getData();
 
-            instaMemberService.connect(memberUser2, "insta_user2", "M");
-            instaMemberService.connect(memberUser3, "insta_user3", "W");
-            instaMemberService.connect(memberUser4, "insta_user4", "M");
-            instaMemberService.connect(memberUser4, "insta_user5", "W");
+                instaMemberService.connect(memberUser2, "insta_user2", "M");
+                instaMemberService.connect(memberUser3, "insta_user3", "W");
+                instaMemberService.connect(memberUser4, "insta_user4", "M");
+                instaMemberService.connect(memberUser4, "insta_user5", "W");
 
-            likeablePersonService.like(memberUser3, "insta_user4", 1);
-            likeablePersonService.like(memberUser3, "insta_user100", 2);
+                likeablePersonService.like(memberUser3, "insta_user4", 1);
+                likeablePersonService.like(memberUser3, "insta_user100", 2);
+            }
              };
     }
 }
