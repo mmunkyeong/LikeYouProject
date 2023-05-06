2ade9d7705dbccb73fc2365c92e18e10c7f39629
mmunkyeong <jmk4379@naver.com>
2023-05-06 23:24:41 +0900

    feat: hasUnreadNotifications TC

--- src/test/java/com/ll/gramgram/boundedContext/notification/controller/NotificationControllerTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/notification/controller/NotificationControllerTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,12 +16,16 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
-
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest // 스프링부트 관련 컴포넌트 테스트할 때 붙여야 함, Ioc 컨테이너 작동시킴
 @AutoConfigureMockMvc // http 요청, 응답 테스트
@@ -57,5 +62,38 @@ public class NotificationControllerTests {
                 .count();
 
         assertThat(unreadCount).isEqualTo(0);
+    }
+    @Test
+    @DisplayName("아직 읽지 않은 알림이 있을 때 상단바에 인디케이터 표시")
+    @WithUserDetails("user4")
+    void t002() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/usr/home/about"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().string(containsString("""
+                        data-test="hasUnreadNotifications"
+                        """.stripIndent().trim())));
+    }
+
+    @Test
+    @DisplayName("아직 읽지 않은 알림이 있을 때, 알림리스트에 접속하면 상단바에 인디케이터 표시가 더 이상 안됨")
+    @WithUserDetails("user4")
+    void t003() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/usr/notification/list"))
+                .andDo(print());
+
+        // THEN
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().string(not(containsString("""
+                        data-test="hasUnreadNotifications"
+                        """.stripIndent().trim()))));
     }
 }
